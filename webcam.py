f8315200b997e70effb2c45049e1219ac0c66832
adrtse <adrian.tse.email@gmail.com>
2025-09-19 18:42:43 -0500

    Update webcam.py
    Added note for webcam

--- webcam.py
+++ webcam.py
@@ -5,7 +5,7 @@ import cv2 as cv
 model = YOLO('yolov8n.pt')
 
 # Initialize webcam
-cap = cv.VideoCapture(1)
+cap = cv.VideoCapture(0) #NOTE: If you have multiple webcam devices and it is reading the wrong webcam, try changing the video index to 1, 2,... until you get the correct one
 
 while True:
     ret, frame = cap.read()
